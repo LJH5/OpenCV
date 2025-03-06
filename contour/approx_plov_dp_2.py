49c48f77ec17aacc522e23e799e360078359db1d
ljh <sigma6317@gmail.com>
2025-03-06 10:56:08 +0900

    study_contour detect

--- contour/approx_plov_dp_2.py
+++ contour/approx_plov_dp_2.py
@@ -4,6 +4,7 @@ import numpy as np
 # 이미지 불러오기
 image = cv2.imread('contour/image/relay.png')
 image = cv2.resize(image, (500, 500))
+cv2.imshow("image", image)
 
 image_copy = image.copy()
 
@@ -25,17 +26,24 @@ cv2.imshow("Gradient Magnitude", magnitude)
 image_canny = cv2.Canny(magnitude, 0, 50)
 cv2.imshow("image_canny", image_canny)
 
+# 엣지 연결을 위한 팽창 연산 적용
+kernel = np.ones((3,3), np.uint8)
+edges_dilated = cv2.dilate(image_canny, kernel, iterations=5)
+cv2.imshow("Edge Dilated", edges_dilated)
+
+# 닫힘 연산 적용
+edges_closed = cv2.morphologyEx(edges_dilated, cv2.MORPH_CLOSE, kernel)
+cv2.imshow("Edge Closed", edges_closed)
 
 # 윤곽선 검출
-contours, _ = cv2.findContours(image_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(edges_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 for contour in contours:
     # contour을 리스트로 넣어 윤곽선 한번에 다 그리기
     cv2.drawContours(image, [contour], -1, (255, 0, 0), 2)
 
     # 윤곽선 단순화, epsilon 값 작을수록 촘촘
-    epsilon = 0.005 * cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, epsilon, True)
+    approx = cv2.approxPolyDP(contour, 30, True)
 
     # 단순화된 윤곽선 그리기 (초록색)
     cv2.drawContours(image, [approx], -1, (0, 255, 0), 2)

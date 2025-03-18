33156b8027ed26a877b013700082362cc1b36915
ljh <sigma6317@gmail.com>
2025-03-18 10:39:32 +0900

    study_threshold

--- contour/adaptive_threshold_1.py
+++ contour/adaptive_threshold_1.py
@@ -1,50 +1,20 @@
 import cv2
-import numpy as np
 
 image = cv2.imread('contour/image/cat.png', cv2.IMREAD_GRAYSCALE)
 image = cv2.resize(image, (500, 500))
 
-_, image_bin = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
-image_bin = cv2.bitwise_not(image_bin)
+_, threshold = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
+threshold = cv2.bitwise_not(threshold)
 
 adaptive_threshold = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 adaptive_threshold = cv2.bitwise_not(adaptive_threshold)
 
-contours_1, _ = cv2.findContours(image_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+contours_1, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 contours_2, _ = cv2.findContours(adaptive_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-h, w = image.shape[:2]
-# 이미지 크기의 검은 배경 만들기, 3채널, 픽셀값 8비트 정수로 저장
-dst_1 = np.zeros((h, w, 3), np.uint8)
-dst_2 = np.zeros((h, w, 3), np.uint8)
-# 흰 배경
-wst_1 = np.ones((h, w, 3), np.uint8) * 255
-wst_2 = np.ones((h, w, 3), np.uint8) * 255
-
-for i in range(len(contours_1)):
-    #BGR
-    bgr_color = (194, 117, 0)
-    # 검은 배경에 외곽선 그리기
-    cv2.drawContours(dst_1, contours_1, i, bgr_color, 2, cv2.LINE_AA)
-    # 흰 배경에 외곽선 그리기
-    cv2.drawContours(wst_1, contours_1, i, bgr_color, 2, cv2.LINE_AA)
-
-for i in range(len(contours_2)):
-    #BGR
-    bgr_color = (194, 117, 0)
-    # 검은 배경에 외곽선 그리기
-    cv2.drawContours(dst_2, contours_2, i, bgr_color, 2, cv2.LINE_AA)
-    # 흰 배경에 외곽선 그리기
-    cv2.drawContours(wst_2, contours_2, i, bgr_color, 2, cv2.LINE_AA)
-
 cv2.imshow('image', image)
-cv2.imshow('image_bin', image_bin)
+cv2.imshow('threshold', threshold)
 cv2.imshow('adaptive_threshold', adaptive_threshold)
-
-cv2.imshow('dst_1', dst_1)
-cv2.imshow('wst_1', wst_1)
-cv2.imshow('dst_2', dst_2)
-cv2.imshow('wst_2', wst_2)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
--- contour/threshold_1.py
+++ contour/threshold_1.py
@@ -4,12 +4,25 @@ import numpy as np
 image = cv2.imread('contour/image/cat.png', cv2.IMREAD_GRAYSCALE)
 image = cv2.resize(image, (500, 500))
 
-_, image_bin_1 = cv2.threshold(image, 175, 255, cv2.THRESH_OTSU)
-_, image_bin_2 = cv2.threshold(image, 175, 255, cv2.THRESH_BINARY)
-_, image_bin_3 = cv2.threshold(image, 175, 255, cv2.THRESH_MASK)
-_, image_bin_4 = cv2.threshold(image, 175, 255, cv2.THRESH_TOZERO)
-_, image_bin_5 = cv2.threshold(image, 175, 255, cv2.THRESH_TRIANGLE)
-_, image_bin_6 = cv2.threshold(image, 175, 255, cv2.THRESH_TRUNC)
+# cv2.threshold(src, thresh, maxval, type, dst=None)
+
+# cv2.THRESH_OTSU: thresh 자동으로 처리, thresh 이상 maxval로 변경
+ret_1, image_bin_1 = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
+
+# cv2.THRESH_BINARY: thresh보다 큰 픽셀은 maxval, 작은 픽셀은 0
+ret_2, image_bin_2 = cv2.threshold(image, 50, 255, cv2.THRESH_BINARY)
+
+# cv2.THRESH_MASK: 흑색 이미지로 변경
+ret_3, image_bin_3 = cv2.threshold(image, 0, 255, cv2.THRESH_MASK)
+
+# cv2.THRESH_TOZERO: thresh보다 작은 픽셀은 0
+ret_4, image_bin_4 = cv2.threshold(image, 80, 255, cv2.THRESH_TOZERO)
+
+# cv2.THRESH_TRIANGLE: triangle 알고리즘, thresh 자동으로 처리, thresh 이상 maxval로 변경
+ret_5, image_bin_5 = cv2.threshold(image, 0, 255, cv2.THRESH_TRIANGLE)
+
+# cv2.THRESH_TRUNC: thresh보다 높은 픽셀을 thresh로 변경, 이하는 원본값
+ret_6, image_bin_6 = cv2.threshold(image, 200, 255, cv2.THRESH_TRUNC)
 
 
 cv2.imshow('Orign', image)
